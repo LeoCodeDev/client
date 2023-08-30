0282e1d260f3926477097d6e259d0e2c66e9f54f
LeoCodeDev <alb.mar.cta@gmail.com>
2023-08-29 19:57:52 -0500

    feat(reducer): complete reducer configuration
    Finished configuring the Redux reducer file. Defined necessary action cases and set the appropriate initial state.

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,13 +1,76 @@
-import { GET_RECIPES, FIND_RECIPE, SHOW_RECIPE, FILTER, ORDER, POST_RECIPE } from "./actions";
+import {
+  FILTER,
+  FIND_RECIPE,
+  GET_RECIPES,
+  ORDER,
+  POST_RECIPE,
+  SHOW_RECIPE,
+} from "./actions";
 
 const initialState = {
-  recipes: [],
-  recipe: {},
+  filter: "all",
   filteredRecipes: [],
   order: "asc",
-  filter: "all",
+  recipe: {},
+  recipes: [],
 };
 
-const rootReducer = () => {};
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_RECIPES:
+      return {
+        ...state,
+        recipes: action.payload,
+        filteredRecipes: action.payload,
+      };
+
+    case FIND_RECIPE:
+      return { ...state, recipe: action.payload };
+
+    case SHOW_RECIPE:
+      return { ...state, recipe: action.payload };
+
+    case FILTER:
+      let filtered = [...state.filteredRecipes];
+      const selectedDiets = [...action.payload];
+      if (selectedDiets.includes("all"))
+        return { ...state, filteredRecipes: filtered };
+
+      return {
+        ...state,
+        filteredRecipes: filtered.filter((recipe) =>
+          recipe.diets.some((diet) => selectedDiets.includes(diet))
+        ),
+      };
+
+    case ORDER:
+      let ordered = [...state.filteredRecipes];
+      const order = {
+        abc: (a, b) => a.name.localeCompare(b.name),
+        asc: (a, b) => a.healthScore - b.healthScore,
+        des: (a, b) => b.healthScore - a.healthScore,
+        zyx: (a, b) => b.name.localeCompare(a.name),
+      };
+
+      const orderFunc = order[action.payload];
+
+      if (action.payload !== "all") ordered.sort(orderFunc);
+
+      return {
+        ...state,
+        filteredRecipes: [...ordered],
+      };
+
+    case POST_RECIPE:
+      return {
+        ...state,
+        recipes: [...state.recipes, action.payload],
+        recipe: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
 
 export { rootReducer, initialState };
